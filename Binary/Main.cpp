4d5956c0046011a68faf439477c481f645fb0a72
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-10-11 16:09:00 -0500

    Binary Math with examples

--- Binary/Main.cpp
+++ Binary/Main.cpp
@@ -77,3 +77,103 @@
 //	  a binary number.
 //	| 10  | 0 | 0 | 0 | 0 | 1 | 0 | 1 | 0 |
 //  | -10 | 1 | 1 | 1 | 1 | 0 | 1 | 0 | 1 |
+
+//Two's Complement
+//	- How computers represent negative numbers today
+//	- Invert the values 
+//	- add binary 1
+//	| 5  |		   | 0 | 0 | 0 | 0 | 0 | 1 | 0 | 1 |
+//  |    | invert  | 1 | 1 | 1 | 1 | 1 | 0 | 1 | 0 |
+//	|    |  add 1  | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 1 |
+//	| -5 |         | 1 | 1 | 1 | 1 | 1 | 0 | 1 | 1 |
+//
+//  | -5 |         | 1 | 1 | 1 | 1 | 1 | 0 | 1 | 1 |
+//  |    | invert  | 0 | 0 | 0 | 0 | 0 | 1 | 0 | 0 |
+//	|    |  add 1  | 0 | 0 | 0 | 0 | 0 | 0 | 0 | 1 |
+//  | 5  |		   | 0 | 0 | 0 | 0 | 0 | 1 | 0 | 1 |
+
+//Bitwise Operators
+//	- Bitwise operators operate on intergral types, treating them as a collection of bits
+//	- & (bitwise AND)
+//		- operand & operand
+//	- | (bitwiase Or)
+//		- operand | operand
+//	- ~ (bitwise NOT)
+//		- ~operand
+//	- ^ (bitwise XOR)
+//		- operand ^ operand
+//	- << (left shift)
+//		- operand << operand
+//	- >> (right shift)
+//		- operand >> operand
+
+//Bitwise AND(&)
+//	- compares two bits
+//	- returns a value of 1 if both bits are set, 0 if they or not.
+//	| 255 |   | 1 | 1 | 1 | 1 | 1 | 1 | 1 | 1 |
+//  | 170 | & | 1 | 0 | 1 | 0 | 1 | 0 | 1 | 0 |
+//  | 170 | = | 1 | 0 | 1 | 0 | 1 | 0 | 1 | 0 |
+int BitwiseAND(int lhs, int rhs)
+{
+	int result = lhs & rhs;
+	return result;
+}
+
+//Bitwise OR(|)
+//	- compares two bits
+//	- returns a value of 1 if either bit is set, 0 if they are not
+//	| 204 |   | 1 | 1 | 0 | 0 | 1 | 1 | 0 | 0 |
+//  | 170 | | | 1 | 0 | 1 | 0 | 1 | 0 | 1 | 0 |
+//  | 238 | = | 1 | 1 | 1 | 0 | 1 | 0 | 1 | 0 |
+int BitwiseOR(int lhs, int rhs)
+{
+	return lhs | rhs;
+}
+
+//Bitwise NOT(~)
+//	- flips each bit
+//	- If bit's value is 1 it becomes 0 and if it's value was 0 it becomes 1
+//	| 204 |   | 1 | 1 | 0 | 0 | 1 | 1 | 0 | 0 |
+//  | 51  | ~ | 0 | 0 | 1 | 1 | 0 | 0 | 1 | 1 |
+int BitwiseNOT(int lhs)
+{
+	return ~lhs;
+}
+
+//Bitwise XOR(^)
+//	- exculisive or, compares two bits
+//	- returns 1 if both bits are different, 0 if they are the same
+//	| 204 |   | 1 | 1 | 0 | 0 | 1 | 1 | 0 | 0 |
+//  | 170 | ^ | 1 | 0 | 1 | 0 | 1 | 0 | 1 | 0 |
+//  | 102 | = | 0 | 1 | 1 | 0 | 0 | 1 | 1 | 0 |
+int BitwiseXOR(int lhs, int rhs)
+{
+	return (lhs ^ rhs);
+}
+
+//Bitmasks
+//	- used to perform a single bitwise operation on a set of bits
+//		- often used for multiple boolean values
+//	- can be used to turn on and off various bits within a set
+//		- also to check the state of a bit within a set
+
+//Bit Shifting
+// - The << and  >> operators shift bits to the left of right, by a value
+int BitshiftLeft(int val, int shift)
+{
+	return val << shift;
+}
+
+int BitshiftRight(int val, int shift)
+{
+	return val >> shift;
+}
+void main()
+{
+	int a = BitwiseAND(5, 6); //4
+	int b = BitwiseOR(5, 6); //7
+	int c = BitwiseNOT(6); //-7
+	int d = BitwiseXOR(5, 6); //
+	int e = BitshiftLeft(5, 2);
+	system("pause");
+}
