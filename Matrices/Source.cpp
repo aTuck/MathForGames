87bbc9598b94043b400b3a8150d1fe88ac5c4a61
Dylan Guidry <dylan.guidry@aie.edu.au>
2017-10-17 12:19:19 -0500

    Matrix

--- Matrices/Source.cpp
+++ Matrices/Source.cpp
@@ -52,13 +52,39 @@ public:
 		}
 		return Matrix2(newMat);
 	}
+
+	Matrix2 operator*(Matrix2 &other)
+	{
+		float newMat[2][2];
+		newMat[0][0] = (mMatrix[0][0] * other.mMatrix[0][0]) +
+					   (mMatrix[0][1] * other.mMatrix[1][0]);
+		newMat[0][1] = (mMatrix[0][0] * other.mMatrix[0][1]) +
+					   (mMatrix[0][1] * other.mMatrix[1][1]);
+		newMat[1][0] = (mMatrix[1][0] * other.mMatrix[0][0]) +
+					   (mMatrix[1][1] * other.mMatrix[1][0]);
+		newMat[1][1] = (mMatrix[1][0] * other.mMatrix[0][1]) +
+					   (mMatrix[1][1] * other.mMatrix[1][1]);
+		return Matrix2(newMat);
+	}
+
+	Matrix2 operator*(float other)
+	{
+		float newMat[2][2];
+		newMat[0][0] = (mMatrix[0][0] * other);
+		newMat[0][1] = (mMatrix[0][1] * other);
+		newMat[1][0] = (mMatrix[1][0] * other);
+		newMat[1][1] = (mMatrix[1][1] * other);
+		return Matrix2(newMat);
+	}
 };
 
 int main()
 {
-	float matA[2][2] = { {2,2}, {2,2} };
-	float matB[2][2] = { { 1,2 },{ 1,2 } };
+	float matA[2][2] = { {1,2}, {3,4} };
+	float matB[2][2] = { { 2,0 },{ 1,2 } };
 	Matrix2 a = Matrix2(matA) + Matrix2(matB);
 	Matrix2 b = Matrix2(matA) - Matrix2(matB);
+	Matrix2 c = Matrix2(matA) * Matrix2(matB);
+	Matrix2 d = Matrix2(matA) * 2;
 	system("pause");
 }
