6826b316b2ffa14bfd217463601c201ae5344d76
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-11-14 09:38:40 -0600

    Grading Matthew Tutorial

---
+++ TestLibrary/Math.cpp
@@ -0,0 +1,21 @@
+#include "Math.h"
+
+float Math::Add(float lhs, float rhs)
+{
+	return lhs + rhs;
+}
+
+float Math::Subtract(float lhs, float rhs)
+{
+	return lhs - rhs;
+}
+
+float Math::Divide(float lhs, float rhs)
+{
+	return lhs / rhs;
+}
+
+float Math::Multiply(float lhs, float rhs)
+{
+	return lhs * rhs;
+}
---
+++ TestLibrary/Math.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class Math
+{
+	float Add(float lhs, float rhs);
+	float Subtract(float lhs, float rhs);
+	float Multiply(float lhs, float rhs);
+	float Divide(float lhs, float rhs);
+};
