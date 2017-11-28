d88507fb89b4618a3bf467517933f2ca983044b7
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-11-28 12:58:36 -0600

    1/4/17 Year 1 exercises

--- Review/Source.cpp
+++ Review/Source.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <math.h>
+
+
+//11/27/17
 void Reverse(char *rev)
 {
 	int length = 0;
@@ -62,33 +65,93 @@ public:
 	}
 };
 
-int main()
+//12/4/17
+int MostCommonValue(int *arr, int numElements)
 {
-	int ar[50];
-	AllEven(ar, 50);
+	int timeSeen = 0;
+	int mostTimeSeen = 0;
+	int commonValue = arr[0];
 
-	char str[] = "Dylan";
-	Reverse(str);
-
-	float a[] = { 1.0f, 1.0f };
-	float b[] = { 1.0f, 1.0f };
-	float *sumVec2 = SumOfVec2(a, b);
-
-	float vec[3] = { 7,3,4 };
-	float ans = MagnitureOfvec3(vec);
-
-	float matA[4] = { 2,2,2,2 };
-	float matB[4] = { 1,1,1,1 };
-	float *matC = MatrixMinusMatrix(matA, matB);
-
-	Cat cats[26];
-	int i = 0;
-	int letter = 65;
-	while (i < 26)
+	for(int i = 0; i < numElements; i++)
 	{
-		cats[i].NameMe((i % 2 == 0) ? letter : letter + 32);
-		letter++;
+		for(int j = 0; j < numElements; j++)
+		{
+			if(arr[i] == arr[j])
+			{
+				timeSeen++;
+			}
+		}
+		if(timeSeen > mostTimeSeen)
+		{
+			mostTimeSeen = timeSeen;
+			commonValue = arr[i];
+		}
+		timeSeen = 0;
+	}
+	return commonValue;
+}
+
+int *CharToInt(char *word)
+{
+	int i = 0;	
+	while(word[i] != '\0')
+	{
 		i++;
 	}
+	int *arr = new int[i];
+	for(int j = 0; j < i; j++)
+	{
+		arr[j] = word[j];
+	}
+	return arr;
+}
+
+//Can't use an if statement
+int LargestValue(int *arr, int len)
+{
+	int largest = arr[0];
+	for(int i = 0; i < len; i++)
+	{
+		largest = (arr[i] > largest) ? arr[i] : largest;
+	}
+	return largest;
+}
+
+
+
+int main()
+{
+	//int ar[50];
+	//AllEven(ar, 50);
+	//char str[] = "Dylan";
+	//Reverse(str);
+	//float a[] = { 1.0f, 1.0f };
+	//float b[] = { 1.0f, 1.0f };
+	//float *sumVec2 = SumOfVec2(a, b);
+	//float vec[3] = { 7,3,4 };
+	//float ans = MagnitureOfvec3(vec);
+	//float matA[4] = { 2,2,2,2 };
+	//float matB[4] = { 1,1,1,1 };
+	//float *matC = MatrixMinusMatrix(matA, matB);
+	//Cat cats[26];
+	//int i = 0;
+	//int letter = 65;
+	//while (i < 26)
+	//{
+	//	cats[i].NameMe((i % 2 == 0) ? letter : letter + 32);
+	//	letter++;
+	//	i++;
+	//}
+	
+	int arr[6] = { 1,2,2,3,2,1 };
+	int c = MostCommonValue(arr, 6);
+
+	int *a = CharToInt("Dylan");
+	for (int i = 0; i < 5; i++)
+		std::cout << a[i];
+
+	int b[5] = { 0,-1,-2,2,-5 };
+	int large = LargestValue(b, 5);
+
 	system("pause");
 }
